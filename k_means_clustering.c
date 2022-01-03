f372e870ebca8e867324051feba277af601447f0
Edoardo Vacchini <edoardo.vacchini01@universitadipavia.it>
2022-01-03 11:44:08 +0100

    Add functions to read the dataset and to calculate the distace between two points

--- k_means_clustering.c
+++ k_means_clustering.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include<string.h>
 
 #define N_COORDINATES 2
 #define N_CLUSTERS 5
+#define PATH_LENGTH 100
+#define BUFFER_LENGTH 1000
 
 typedef struct {
     double coordinates[N_COORDINATES];
@@ -12,6 +15,7 @@ typedef struct {
     double coordinates[N_COORDINATES];
     unsigned int clusterId;
 } centroid_t;
+
 
 centroid_t *initCentroids(point_t *points, unsigned int nPoints, unsigned int nClusters) {
     centroid_t *centroids = NULL;
@@ -37,6 +41,55 @@ centroid_t *initCentroids(point_t *points, unsigned int nPoints, unsigned int nC
 
     return centroids;
 }
+
+
+point *readDataset(int *nPoints, char path[PATH_LENGTH]) {
+    FILE *file;
+    if (!( file = fopen(path, "r") ))
+        return NULL;
+
+    // The first line of the file contains the number of points in the file
+    // the array of points is prepared accordingly so
+    point_t *points;
+    char buffer[BUFFER_LENGTH];
+    fgets(buffer, sizeof(buffer), file);
+    sscanf(buffer, "%d", nPoints);
+    if (!( points = malloc((*nPoints) * sizeof(*points)) ))
+        return NULL;
+
+    // For all the lines, it reads them, tokenize them one token by one
+    // token and if the number of token is different from the number of 
+    // dimensions stops the procedure and returns NULL
+    int converted, i;
+    char delimiter = ' ', *token = NULL;
+    for ( i = 0; fgets(buffer, sizeof(buffer), file); i++ ) {
+        converted = 0;
+        token = strtok( buffer, delimiter );
+        while( token != NULL ) {
+            token = strtok( NULL, delimiter );
+            points[i].coordinates[converted] = atof(token);
+            converted ++;
+        }   
+        if (converted != N_COORDINATES){
+            free(points);
+            return NULL;
+        }
+    }
+
+    return points;
+}
+
+
+double squaredDistance(point_t *point, centroid_t *centroid){
+    int i, distance;
+    double squaredDistance = 0;
+    for(i = 0; i < N_COORDINATES; i++){
+        distance = point->coordiantes[i] - centroid->coordinate[i];
+        squaredDistance += distance * distance;
+    }
+    return squaredDistance;
+}
+
 
 int main() {
     
