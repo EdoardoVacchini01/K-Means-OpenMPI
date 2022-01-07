38d2b7e531c8b429c41ff60ed767e30456cb2f81
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2022-01-07 17:18:13 +0100

    Complete the implementation of initDatatypes()

--- k_means_clustering.c
+++ k_means_clustering.c
@@ -218,13 +218,23 @@ unsigned int readDataset(const char *path, point_t **points) {
 
 void initDatatypes(MPI_Datatype *pointDatatype, MPI_Datatype *centroidDatatype, MPI_Datatype *prototypeDatatype) {
     point_t point = {0};
+    prototype_t prototype = {0};
+    int pointBlockLengths[] = {DIMENSION, 1};
+    int centroidBlockLengths[] = {DIMENSION};
+    int prototypeBlockLengths[] = {DIMENSION, 1};
+    MPI_Aint pointIndices[] = {0, (void*)&point.clusterId - (void*)&point};
+    MPI_Aint centroidIndices[] = {0};
+    MPI_Aint prototypeIndices[] = {0, (void*)&prototype.nPoints - (void*)&prototype};
+    MPI_Datatype pointTypes[] = {MPI_DOUBLE, MPI_UNSIGNED};
+    MPI_Datatype centroidTypes[] = {MPI_DOUBLE};
+    MPI_Datatype prototypeTypes[] = {MPI_DOUBLE, MPI_UNSIGNED};
 
-    int blockLengths[] = {DIMENSION, 1};
-    MPI_Aint indices[] = {0, (void*)&point.clusterId - (void*)&point};
-    MPI_Datatype types[] = {MPI_DOUBLE, MPI_UNSIGNED};
-
-    MPI_Type_create_struct(2, blockLengths, indices, types, pointDatatype);
+    MPI_Type_create_struct(2, pointBlockLengths, pointIndices, pointTypes, pointDatatype);
+    MPI_Type_create_struct(1, centroidBlockLengths, centroidIndices, centroidTypes, centroidDatatype);
+    MPI_Type_create_struct(2, prototypeBlockLengths, prototypeIndices, prototypeTypes, prototypeDatatype);
     MPI_Type_commit(pointDatatype);
+    MPI_Type_commit(centroidDatatype);
+    MPI_Type_commit(prototypeDatatype);
 }
 
 
