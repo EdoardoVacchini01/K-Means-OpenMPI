b9a2b61632b6f45921c7fcb3dc464cb1e7c02a36
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2022-02-09 11:12:10 +0100

    Remove warning triggered by scikit-learn's KMeans when passing both init points and an n_init value greater than one

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -7,9 +7,9 @@ import sys
 if __name__ == '__main__':
     points = pd.read_csv(sys.argv[1], sep=' ', header=None, skiprows=[0])
 
-    k_means = KMeans(n_clusters=int(sys.argv[2]), max_iter=100,
-        init=points.iloc[0:int(sys.argv[2])], tol=0.0, algorithm='full')
-    fitted_k_means = k_means.fit(points);
+    k_means = KMeans(n_clusters=int(sys.argv[2]), init=points.iloc[0:int(sys.argv[2])], n_init=1,
+        max_iter=100, tol=0.0, algorithm='full')
+    fitted_k_means = k_means.fit(points)
 
     with open(sys.argv[3], 'w') as outputFile:
         for centroid in fitted_k_means.cluster_centers_:
