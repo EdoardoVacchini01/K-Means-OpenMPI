ef256e10992a8e01342b5f909285f7f76c115970
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2022-02-12 23:07:12 +0100

    Add exception handling in k_means_clustering.py (same checks as k_means_clustering.c)
    (cherry picked from commit 0d1018fa670cb8681e9ee504b17c033db1b16887)

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -7,11 +7,20 @@ import sys
 if __name__ == '__main__':
     # Read the data points of the dataset
     print('Reading the dataset file...')
-    points = pd.read_csv(sys.argv[1] if len(sys.argv) > 1 else 'dataset.txt', sep=' ', header=None,
-        skiprows=[0])
+    try:
+        points = pd.read_csv(sys.argv[1] if len(sys.argv) > 1 else 'dataset.txt', sep=' ',
+            header=None, skiprows=[0])
+    except:
+        print('An error occurred while reading the dataset file.');
+        sys.exit(1)
 
     # Get the number of clusters to identify in the dataset
     n_clusters = int(sys.argv[3]) if len(sys.argv) > 3 else 3
+
+    # Check if the number of clusters exceeds the number of data points of the dataset
+    if n_clusters > len(points.index):
+        print('The number of clusters must be less than or equal to the number of data points.');
+        sys.exit(1)
 
     # Get the maximum number of iterations to perform
     max_iterations = int(sys.argv[4]) if len(sys.argv) > 4 else 100
@@ -28,6 +37,9 @@ if __name__ == '__main__':
         print(' '.join([f'{coordinate:.10f}' for coordinate in centroid]))
 
     # Print the centroids to the output file
-    with open(sys.argv[2] if len(sys.argv) > 2 else 'centroids.txt', 'w') as output_file:
-        for centroid in fitted_k_means.cluster_centers_:
-            print(' '.join([f'{coordinate:.10f}' for coordinate in centroid]), file=output_file)
+    try:
+        with open(sys.argv[2] if len(sys.argv) > 2 else 'centroids.txt', 'w') as output_file:
+            for centroid in fitted_k_means.cluster_centers_:
+                print(' '.join([f'{coordinate:.10f}' for coordinate in centroid]), file=output_file)
+    except:
+        print('\nAn error occurred while opening the output file.');
